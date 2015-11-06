3d2fe789d921198825284261db81f227a2376e5e
Your Actual Name <el.dyko@gmail.com>
2015-11-06 16:57:26 -0500

    added smooth scroll and more styling

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ $(function(){
 	console.log("It's working");
 
 });
+
+/* 
+ Smooth Scroll to Element
+ */
+jQuery(document).ready(function(){
+    jQuery('a[href^="#"]').click(function(e) {
+        jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
+        return false;
+        e.preventDefault();
+    });
+});
